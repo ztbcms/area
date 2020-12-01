09538001226ff0ea6d12c10b156eeb3b2a289048
Jayin Ton <tonjayin@gmail.com>
2020-12-01 18:11:14 +0800

    增加缓存接口

--- controller/Api.php
+++ controller/Api.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: zhlhuang
- * Date: 2020-09-19
- * Time: 11:19.
  */
 
 namespace app\area\controller;
@@ -11,22 +8,30 @@ namespace app\area\controller;
 
 use app\area\model\AreaModel;
 use app\BaseController;
+use think\facade\Cache;
 
 class Api extends BaseController
 {
     /**
      * 获取地区树形结构
-     * @throws \think\db\exception\DataNotFoundException
+     *
+     * @return \think\response\Json
      * @throws \think\db\exception\DbException
      * @throws \think\db\exception\ModelNotFoundException
-     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
      */
     function getAreaTree()
     {
+        $cache_key = __CLASS__.__METHOD__;
+        var_dump($cache_key);
+        $res = Cache::get($cache_key);
+        if ($res) {
+            return json(self::createReturn(true, $res));
+        }
         $areas = AreaModel::where('pcode', 0)->field(['area_name', 'code', 'pcode'])
             ->with(['children.children'])
-            ->cache(3600)
-            ->select();
-        return json(self::createReturn(true, $areas, 'ok'));
+            ->select()->toArray();
+        Cache::set($cache_key, $areas);
+        return json(self::createReturn(true, $areas));
     }
 }
